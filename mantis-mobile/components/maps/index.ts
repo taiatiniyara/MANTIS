847d605a8b2c9a1d40b8246b430aab84ec2c56e8
taiatiniyara <taiatiniyara@gmail.com>
2025-10-15 11:03:42 +1200

    wedwed

--- mantis-mobile/components/maps/index.ts
+++
@@ -1,4 +0,0 @@
-// Maps components barrel export
-export { InfringementMapView } from './infringement-map-view';
-export { LocationPicker } from './location-picker';
-export * from './map-styles';
--- mantis-web/src/components/maps/index.ts
+++
@@ -1,5 +0,0 @@
-export { InfringementMapView } from './infringement-map-view';
-export { InfringementHeatmap } from './infringement-heatmap';
-export { LocationPicker } from './location-picker';
-export { MapSkeleton } from './map-skeleton';
-export { StaticMapImage } from './static-map-image';
