b85040201e34ffbe762fc7477e71a1aca2da45d4
taiatiniyara <taiatiniyara@gmail.com>
2026-01-13 11:54:16 +1200

    qqdd

--- web/app/admin/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { DashboardSkeleton } from "@/components/loading-skeleton";
-import React from "react";
-
-export default function Loading() {
-  return <DashboardSkeleton />;
-}
--- web/app/protected/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { DashboardSkeleton } from "@/components/loading-skeleton";
-
-export default function Loading() {
-  return <DashboardSkeleton />;
-}
