a0e137f1c5f71398c433b640d944aeb0cf1eb2a4
taiatiniyara <taiatiniyara@gmail.com>
2026-01-14 05:33:57 +1200

    'deploy'

--- website/src/routes/super-admin/users/index.tsx
+++ website/src/routes/super-admin/users/index.tsx
@@ -1,9 +1,42 @@
-import { createFileRoute } from '@tanstack/react-router'
+import SupabaseDataLoader from "@/components/supabaseLoader";
+import { Badge } from "@/components/ui/badge";
+import { H3 } from "@/components/ui/heading";
+import { useSupabaseQuery } from "@/hooks/useSupabaseQuery";
+import { type User } from "@/lib/supabase/schema";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/super-admin/users/')({
+export const Route = createFileRoute("/super-admin/users/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/super-admin/users/"!</div>
+  const list = useSupabaseQuery<User>({
+    tableName: "users",
+    queryKey: ["super-admin", "users", "list"],
+  });
+  return (
+    <div>
+      <H3>Users</H3>
+
+      <SupabaseDataLoader
+        data={list.data}
+        error={list.error}
+        isLoading={list.isLoading}
+      >
+        <p className="text-neutral-500 text-sm">
+          Total Users: <b>{list.data?.length}</b>
+        </p>
+        {list.data?.map((user) => (
+          <div
+            className="border my-2 p-2 shadow bg-white rounded"
+            key={user.id}
+          >
+            {user.display_name}
+            <br />
+            <Badge variant={"outline"}>{user.role}</Badge>
+          </div>
+        ))}
+      </SupabaseDataLoader>
+    </div>
+  );
 }
